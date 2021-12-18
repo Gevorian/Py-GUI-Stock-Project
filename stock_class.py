84af2ed4400779d5ac04ce12324c50276971473e
Gevorian <gevorian@gmx.us>
2021-12-18 12:21:14 -0600

    Add files via upload

---
+++ stock_class.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Anthony Mosqueda
+"""
+
+class Stock:
+    def __init__(self, symbol, name, shares):
+        self.symbol = symbol
+        self.name = name
+        self.shares = shares
+        self.DataList = []
+        
+    def add_data(self, stock_data):
+        self.DataList.append(stock_data)
+        
+class DailyData:
+    def __init__(self, date, close, volume):
+        self.date = date
+        self.close = close
+        self.volume = volume
+        
+        
